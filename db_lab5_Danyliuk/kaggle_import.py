eeac3c641f54a3ff11754f53d9ea2357d6bcf480
Danylian13 <91112378+Danylan13@users.noreply.github.com>
2023-12-25 18:47:18 +0200

    Update kaggle_import.py

--- db_lab5_Danyliuk/kaggle_import.py
+++ db_lab5_Danyliuk/kaggle_import.py
@@ -41,7 +41,7 @@ with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
         cur.execute("INSERT INTO Beer (id_beer, beer_name, type_of_beer, alcohol_content, country_of_origin, price, id_brewery) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id_beer) DO NOTHING",
                     (id_beer, beer_name, type_of_beer, float(alcohol_content), country_of_origin, float(price), id_brewery))
 
-        # Вставка даних в таблицю Beer_reviews (Можливо, доведеться налаштувати цю частину залежно від структури вашого набору даних)
+        # Вставка даних в таблицю Beer_reviews
         cur.execute("INSERT INTO Beer_reviews (id_review, review_date, author, rating, comment, id_beer) VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (id_review) DO NOTHING",
                     (id_beer, '1900-01-01', 'Ім\'я автора', 5.0, 'Коментар', id_beer))
 
